478aee1592dc7ba941037ea818529e45b78a0406
Blake Loring <blake@parsed.uk>
2019-01-18 05:53:11 +0000

    Move out comment meta-data to Meta

---
+++ src/Meta.hs
@@ -0,0 +1,39 @@
+module Meta where
+import Util
+
+extractTitle :: String -> String
+extractTitle source = trim (drop (length prelude) (findLine prelude source))
+  where
+    prelude = "!=!=! Title:"
+
+extractDate :: String -> Float
+extractDate source = read (trim (drop (length prelude) (findLine prelude source))) :: Float
+  where
+    prelude = "!=!=! Created:"
+
+parseTags :: String -> String -> [String]
+parseTags current [] = [current]
+parseTags current (',':xs) = current:(parseTags "" xs)
+parseTags current (x:xs) = parseTags (x:current) xs
+
+extractTags :: String -> [String]
+extractTags source = map trim $ map reverse $ tags 
+  where
+    prelude = "!=!=! Tags:"
+    tags = parseTags "" (trim (drop (length prelude) (findLine prelude source)))
+
+extractIntroduction :: String -> String
+extractIntroduction source = trim intro
+  where
+    comment = "!=!=!"
+    start = comment ++ " Intro: Start"
+    end = comment ++ " Intro: End"
+    portion = untilString "!=!=! Intro: End" (fromString "!=!=! Intro: Start" source)
+    intro = drop (length start) (reverse (drop (length end) (reverse portion))) 
+
+mergeTag :: String -> String -> String
+mergeTag next current = if (length current) == 0 then next else current ++ ", " ++ next
+
+mergeTags tags = foldr mergeTag "" tags
+
+
--- src/main.hs
+++ src/main.hs
@@ -4,9 +4,7 @@ import Control.Monad
 import Data.List 
 import Util
 import CopyDirectory
-
-import Data.List
-import Data.Function
+import Meta
 
 type ArticleInfo = (String, Float, String, [String])
 
@@ -36,41 +34,6 @@ getAllMarkdown root = do
 
 rewriteSuffix :: String -> String
 rewriteSuffix source = replaceInString source ".md" ".html"
-
-extractTitle :: String -> String
-extractTitle source = trim (drop (length prelude) (findLine prelude source))
-  where
-    prelude = "!=!=! Title:"
-
-extractDate :: String -> Float
-extractDate source = read (trim (drop (length prelude) (findLine prelude source))) :: Float
-  where
-    prelude = "!=!=! Created:"
-
-parseTags :: String -> String -> [String]
-parseTags current [] = [current]
-parseTags current (',':xs) = current:(parseTags "" xs)
-parseTags current (x:xs) = parseTags (x:current) xs
-
-extractTags :: String -> [String]
-extractTags source = map trim $ map reverse $ tags 
-  where
-    prelude = "!=!=! Tags:"
-    tags = parseTags "" (trim (drop (length prelude) (findLine prelude source)))
-
-extractIntroduction :: String -> String
-extractIntroduction source = trim intro
-  where
-    comment = "!=!=!"
-    start = comment ++ " Intro: Start"
-    end = comment ++ " Intro: End"
-    portion = untilString "!=!=! Intro: End" (fromString "!=!=! Intro: Start" source)
-    intro = drop (length start) (reverse (drop (length end) (reverse portion))) 
-
-mergeTag :: String -> String -> String
-mergeTag next current = if (length current) == 0 then next else current ++ ", " ++ next
-
-mergeTags tags = foldr mergeTag "" tags
 
 transformArticle :: String -> Int -> (Int, FilePath) -> IO ArticleInfo 
 transformArticle template total (index, filename) = do
