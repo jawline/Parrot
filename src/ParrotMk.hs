87d86957328cc921a8379068b49cab3e3dbd381e
Blake Loring <blake@parsed.uk>
2020-07-04 04:22:59 +0200

    Implement ParrotMk to simplify article creation

--- src/ParrotMk.hs
+++ src/ParrotMk.hs
@@ -1,20 +1,35 @@
 {-# LANGUAGE QuasiQuotes #-}
 import Text.RawString.QQ
-import Data.Time
+import Data.Time (formatTime, getCurrentTime, defaultTimeLocale)
+import System.Environment (getArgs)
+import System.Exit (exitWith, ExitCode(ExitFailure))
+import Control.Monad (when)
 
-multiline :: String
-multiline = [r|!=!=! Title: ${{{ARTICLE_TITLE}}}
-!=!=! Created: ${{{CREATED_ON}}}
-!=!=! Tags: ${{{ARTICLE_TAGS}}}
+articleTemplate :: String -> String
+articleTemplate currentDate = [r|!=!=! Title: Article Title
+!=!=! Created: |] ++ currentDate ++ [r|
+!=!=! Tags: List1, List2, etc
 
 !=!=! Intro: Start
 Put the description / introduction of your article here.
 !=!=! Intro: End
 
-Put the main body of content here.
-|]
+Put the main body of content here.|]
+
+failArguments = do
+  putStrLn "Usage: executable OUTPUT_ARTICLE_FILEPATH"
+  exitWith (ExitFailure 1)
 
 main = do
+  programArguments <- getArgs
+
+  when (length programArguments /= 1) failArguments
+  let targetFile = programArguments !! 0
+
   createdAt <- getCurrentTime
-  putStrLn (show createdAt)
+  let humanReadableTime = (formatTime defaultTimeLocale "%d-%m-%Y" createdAt)
+  let articleTemplate' = articleTemplate humanReadableTime
+
+  writeFile targetFile articleTemplate'
+  putStrLn ("[+] Wrote " ++ targetFile ++ ". Now open this file in your text editor.")
   return ()
